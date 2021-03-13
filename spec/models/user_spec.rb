4ff392f214196a3436d13a3492a8c97d1c819cee
Draculk <kayranvieira456@hotmail.com>
2021-03-13 19:39:05 -0300

    :construction: usando devise

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "factory" do
+    context "Normal" do
+      it "be valid" do
+        user = build(:user)
+        expect(user).to be_valid
+      end
+    end
+  end
+
+  describe "validations" do
+    context "no email" do
+      it {expect(build(:user, email: nil)).to be_invalid}
+    end
+
+    context "no name" do
+      it {expect(build(:user, name: nil)).to be_invalid}
+    end
+
+    context "no registration" do
+      it {expect(build(:user, registration: nil)).to be_invalid}
+    end
+  end
+end
