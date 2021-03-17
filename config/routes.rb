59e2e1676978f2a880a182004159025d7437d29f
Draculk <kayranvieira456@hotmail.com>
2021-03-17 09:27:40 -0300

    :sparkles: mais funcionalidades na controller dos livros

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   scope 'books' do
     get '/new', to: 'books#new', as: :new_book
     post '/new', to: 'books#create'
+    get '/:id', to: 'books#show', as: :show_book
+    get '/:id/edit', to: 'books#edit', as: :edit_book
+    patch '/:id/edit', to: 'books#update'
+    delete '/:id', to: 'books#destroy', as: :delete_book
   end
 end
